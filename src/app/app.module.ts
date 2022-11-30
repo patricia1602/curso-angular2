b4479f87d2e981dd2a13f3190c8fdb694b7c9572
Patricia <patyamiga16@hotmail.com>
2022-11-30 17:44:41 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponet } from "./meu-primeiro/meu-primeiro.component";
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MeuPrimeiroComponet,
+        MeuPrimeiro2Component
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ]
 })
 export class AppModule { }
