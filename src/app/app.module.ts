f092c9e57e34a62114377f036fe7ed3b63676407
Patricia <patyamiga16@hotmail.com>
2022-12-02 11:19:09 -0300

    aula5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponet } from "./meu-primeiro/meu-primeiro.component";
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        CursosModule
     ]
 })
 export class AppModule { }
---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
+@NgModule({
+  declarations: [
+    CursosComponent,
+    CursoDetalheComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CursosComponent
+  ]
+})
+export class CursosModule { }
