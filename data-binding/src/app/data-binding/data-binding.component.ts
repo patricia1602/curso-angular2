e32a028c072b233e6b7b77096059fe8cb4278f67
Patricia <patyamiga16@hotmail.com>
2022-12-06 17:44:54 -0300

    aula11

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,14 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  //styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+    .highlight {
+    background-color: yellow;
+    font-weight: bold;
+    }
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
-
   urlImagem = 'https://static.vecteezy.com/ti/fotos-gratis/t2/6357882-longa-exposicao-exotica-bela-tropical-deep-rainforest-cachoeira-fresco-turquesa-cachoeiras-na-floresta-profunda-de-huay-mae-khamin-cachoeira-no-parque-nacional-bela-paisagem-cachoeiras-gratis-foto.jpg'
+
+  valorAtual: string = '';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
 
   getValor(){
     return 1;
@@ -19,6 +31,22 @@ export class DataBindingComponent implements OnInit {
   getCurtirCurso(){
     return true;
   }
+
+  botaoClicado(){
+    alert('Botao clicado!');
+  }
+
+  OnKeyUp(evento: KeyboardEvent){
+    this.valorAtual = ((<HTMLInputElement>evento.target).value);
+  }
+
+  salvarValor(valor: any) {
+    this.valorSalvo = valor;
+  }
+
+    onMouseOverOut() {
+      this.isMouseOver = !this.isMouseOver;
+    }
 
   constructor() { }
 
