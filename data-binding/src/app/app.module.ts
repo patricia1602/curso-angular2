3784c33a4f536459a9e061d6fdcce01f96b77122
Patricia <patyamiga16@hotmail.com>
2022-12-13 17:33:26 -0300

    aula15

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CicloComponent } from './ciclo/ciclo.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
-import { FormsModule } from '@angular/forms';
-import { MeuFormModule } from './meu-form/meu-form.module';
 import { InputPropertyComponent } from './input-property/input-property.component';
+import { MeuFormModule } from './meu-form/meu-form.module';
 import { OutputPropertyComponent } from './output-property/output-property.component';
 
 @NgModule({
@@ -14,7 +15,8 @@ import { OutputPropertyComponent } from './output-property/output-property.compo
     AppComponent,
     DataBindingComponent,
     InputPropertyComponent,
-    OutputPropertyComponent
+    OutputPropertyComponent,
+    CicloComponent
   ],
   imports: [
     BrowserModule,
